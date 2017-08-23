7712f256c9a6d56f463378ac773efaf134387a7a
Kira <me@imkira.com>
2017-08-23 11:02:27 +0800

    update auto-generated client

--- kubernetes/scheme/register.go
+++ kubernetes/scheme/register.go
@@ -6,20 +6,15 @@ package scheme
 
 import (
 	releasev1alpha1 "github.com/caicloud/clientset/pkg/apis/release/v1alpha1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
-	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	scheme "k8s.io/client-go/kubernetes/scheme"
 )
 
-var Scheme = runtime.NewScheme()
-var Codecs = serializer.NewCodecFactory(Scheme)
-var ParameterCodec = runtime.NewParameterCodec(Scheme)
+var Scheme = scheme.Scheme
+var Codecs = scheme.Codecs
+var ParameterCodec = scheme.ParameterCodec
 
 func init() {
-	v1.AddToGroupVersion(Scheme, schema.GroupVersion{Version: "v1"})
-	scheme.AddToScheme(Scheme)
 	AddToScheme(Scheme)
 }
 
